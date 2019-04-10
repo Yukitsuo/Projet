b1db333861c341455256c3ea53f5b11638b9985a
Yukitsuo <auy@iut.univ-paris8.fr>
2019-04-10 10:34:10 +0200

    Modification

---
+++ src/OffreStage.java
@@ -0,0 +1,17 @@
+
+public class OffreStage {
+	private int ID;
+	private String descriptif;
+	private String titre;
+	private String dateDeb;
+	private double durée;
+	
+	
+	public OffreStage(int ID, String descriptif, String titre, String dateDeb, double durée) {
+		this.ID = ID;
+		this.descriptif = descriptif;
+		this.titre = titre;
+		this.dateDeb = dateDeb;
+		this.durée = durée;
+	}
+}
