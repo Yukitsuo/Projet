353dde49565ac7e02e4df4597888fb23b3b227c3
Yukitsuo <auy@iut.univ-paris8.fr>
2019-04-19 11:13:50 +0200

    envoiefichier

--- src/OffreStage.java
+++ src/OffreStage.java
@@ -1,17 +1,50 @@
 
 public class OffreStage {
-	private int ID;
+	private int id;
 	private String descriptif;
 	private String titre;
 	private String dateDeb;
-	private double durée;
+	private double duree;
 	
 	
-	public OffreStage(int ID, String descriptif, String titre, String dateDeb, double durée) {
-		this.ID = ID;
+	public OffreStage(int id, String descriptif, String titre, String dateDeb, double duree) {
+		this.id = id;
 		this.descriptif = descriptif;
 		this.titre = titre;
 		this.dateDeb = dateDeb;
-		this.durée = durée;
+		this.duree = duree;
 	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescriptif() {
+		return descriptif;
+	}
+
+
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+
+
+	public String getDateDeb() {
+		return dateDeb;
+	}
+
+
+
+
+	public double getDurée() {
+		return duree;
+	}
+
+
+
 }
